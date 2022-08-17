b7dacbc1167bd01874efbecf049bc29eb3d1a69f
crypt0rr <57799908+crypt0rr@users.noreply.github.com>
2022-08-17 15:16:21 +0200

    Version bump

--- hash-cracker.sh
+++ hash-cracker.sh
@@ -2,7 +2,7 @@
 # Copyright crypt0rr
 
 function main () {
-    echo -e "hash-cracker v2.9.2 Apple Silicon Edition by crypt0rr (https://github.com/crypt0rr)\n"
+    echo -e "hash-cracker v2.9.4 Apple Silicon Edition by crypt0rr (https://github.com/crypt0rr)\n"
     echo "Checking if requirements are met:"
     source scripts/requirements.sh
     menu
