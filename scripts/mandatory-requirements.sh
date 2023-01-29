12325563a80f375b59a53e01f02489ffd5cd2cbf
crypt0rr <57799908+crypt0rr@users.noreply.github.com>
2023-01-29 17:06:05 +0100

    Split requirements into two files

--- scripts/mandatory-requirements.sh
+++ scripts/mandatory-requirements.sh
@@ -11,16 +11,6 @@ if ! [ -x "$(command -v $HASHCAT)" ]; then
 else
     echo '[+] Hashcat is installed'
 fi
-if [[ -x "scripts/extensions/common-substr" ]]; then
-    echo '[+] common-substr is executable'
-else
-    echo '[-] common-substr is not executable or found'; ((COUNTER=COUNTER + 1))
-fi
-# if [[ -x "scripts/extensions/hashcat-utils/bin/expander.bin" ]]; then
-#     echo '[+] expander is executable'
-# else
-#     echo '[-] expander is not available/executable or found, this is needed for fingerprint cracking'
-# fi
 if test -f "$POTFILE"; then
     echo '[+] Potfile "hash-cracker.pot" present'
 else
@@ -29,11 +19,4 @@ else
 fi
 if [ "$COUNTER" \> 0 ]; then
     echo -e "\nNot all mandatory requirements are met. Please fix and try again."; exit 1
-fi
-if [ "$NOP" == '-n' ] || [ "$NOP" == '--no-limit' ]; then
-    echo "[-] Optimised kernels disabled"
-    KERNEL=''
-else
-    echo "[+] Optimised kernels enabled"
-    KERNEL='-O'
 fi
---
+++ scripts/optional-requirements.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Copyright crypt0rr
+
+# Requirements
+HASHCAT=$(command -v hashcat)
+POTFILE=(hash-cracker.pot)
+
+# Logic
+if [[ -x "scripts/extensions/common-substr" ]]; then
+    echo '[+] common-substr is executable'
+else
+    echo '[-] common-substr is not executable or found (option 10 / 11)'
+fi
+if [[ -x "scripts/extensions/hashcat-utils/bin/expander.bin" ]]; then
+    echo '[+] expander is executable'
+else
+    echo '[-] expander is not available/executable or found, this is needed for fingerprint cracking'
+fi
+
+echo -e "\nKernel mode:"
+if [ "$NOP" == '-n' ] || [ "$NOP" == '--no-limit' ]; then
+    echo "[-] Optimised kernels disabled"
+    KERNEL=''
+else
+    echo "[+] Optimised kernels enabled"
+    KERNEL='-O'
+fi
