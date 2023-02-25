3be9de4258118f70e1b5ea0bfeb4807387b6b2d7
crypt0rr <57799908+crypt0rr@users.noreply.github.com>
2023-02-25 16:19:04 +0100

    Added temporary file handling and CTRL+C

--- scripts/processors/fingerprint.sh
+++ scripts/processors/fingerprint.sh
@@ -5,9 +5,21 @@
 source scripts/selectors/hashtype.sh
 source scripts/selectors/hashlist.sh
 
+# Cleanup
+function clean_up {
+    rm $tmp $tmp2 2>/dev/null
+    exit
+}
+
+trap clean_up SIGINT SIGTERM
+
+# Temporary Files
+tmp=$(mktemp /tmp/hash-cracker-tmp.XXXX)
+tmp2=$(mktemp /tmp/hash-cracker-tmp.XXXX)
+
 # Logic
-cat $POTFILE | awk -F: '{print $NF}' | sort -u | tee tmp_pwonly &>/dev/null
-./scripts/extensions/hashcat-utils/bin/expander.bin < tmp_pwonly | iconv -f ISO-8859-1 -t UTF-8//TRANSLIT | sort -u > tmp_expanded && rm tmp_pwonly
-$HASHCAT $KERNEL --bitmap-max=24 --potfile-path=$POTFILE -m$HASHTYPE $HASHLIST -a 1 tmp_expanded tmp_expanded
-rm tmp_expanded
+cat $POTFILE | awk -F: '{print $NF}' | sort -u | tee $tmp &>/dev/null
+./scripts/extensions/hashcat-utils/bin/expander.bin < $tmp | iconv -f ISO-8859-1 -t UTF-8//TRANSLIT | sort -u > $tmp2 && rm $tmp
+$HASHCAT $KERNEL --bitmap-max=24 --potfile-path=$POTFILE -m$HASHTYPE $HASHLIST -a 1 $tmp2 $tmp2
+rm $tmp2
 echo -e "\nFingerprint attack done\n"
