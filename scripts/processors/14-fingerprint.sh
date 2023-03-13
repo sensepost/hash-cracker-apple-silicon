b377d2cc21ac4680f6196ae0957eb3fcd20add9f
Bart <57799908+crypt0rr@users.noreply.github.com>
2023-03-13 12:13:01 +0100

    Release v3.4 (#15)
    * Added temporary file handling and CTRL+C
    
    * Version bump
    
    * Update VERSION.md
    
    * Renamed processors to match menu
    
    * Removed tmp_ from .gitignore since no more use
    
    * Added cracklib.txt
    
    * Update VERSION.md
    
    * Update README.md
    
    * Update README.md
    
    * Moved from static loopback to variable
    
    * Removed showinfo.sh
    
    * Removed kernel mode checking from optional checks
    
    * Added parameters.sh handling all parameters
    
    * Moved handling and changed the way of starting
    
    * Update README.md
    
    * Update VERSION.md
    
    * Update README.md
    
    * Changed notation

---
+++ scripts/processors/14-fingerprint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Copyright crypt0rr
+
+# Requirements
+source scripts/selectors/hashtype.sh
+source scripts/selectors/hashlist.sh
+
+# Cleanup
+function clean_up {
+    rm $tmp $tmp2 2>/dev/null
+    exit
+}
+
+trap clean_up SIGINT SIGTERM
+
+# Temporary Files
+tmp=$(mktemp /tmp/hash-cracker-tmp.XXXX)
+tmp2=$(mktemp /tmp/hash-cracker-tmp.XXXX)
+
+# Logic
+cat $POTFILE | awk -F: '{print $NF}' | sort -u | tee $tmp &>/dev/null
+./scripts/extensions/hashcat-utils/bin/expander.bin < $tmp | iconv -f ISO-8859-1 -t UTF-8//TRANSLIT | sort -u > $tmp2 && rm $tmp
+$HASHCAT $KERNEL --bitmap-max=24 --potfile-path=$POTFILE -m$HASHTYPE $HASHLIST -a 1 $tmp2 $tmp2
+rm $tmp2
+echo -e "\nFingerprint attack done\n"
