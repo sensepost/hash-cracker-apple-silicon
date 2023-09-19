17db1d83da9b67d3eb05bdeb6e59cdcec61d419b
Bart <57799908+crypt0rr@users.noreply.github.com>
2023-09-19 17:57:38 +0200

    Release v3.8 (#19)
    * Added WORDLIST 1 and 2
    
    * Version bump
    
    * Added display of static parameters
    
    * Added static config checking and behavior
    
    * Update README.md
    
    * Update VERSION.md
    
    * Update README.md
    
    * Update VERSION.md

--- hash-cracker.sh
+++ hash-cracker.sh
@@ -2,7 +2,7 @@
 # Author: crypt0rr - https://github.com/crypt0rr/
 
 function hash-cracker () {
-    echo -e "\nhash-cracker v3.7 Apple Silicon Edition by crypt0rr (https://github.com/crypt0rr)"
+    echo -e "\nhash-cracker v3.8 Apple Silicon Edition by crypt0rr (https://github.com/crypt0rr)"
 }
 
 function menu () {
